ca6232bbf0fe45ecb85d94f6a6aa7ded36b411ff
Aaron Winter <winter.aaron93@gmail.com>
2019-02-04 15:24:25 +0100

    Revert "added tree to Level1.tscn for the Victory, also started working on the menu"
    This reverts commit 3b575300e4dd5c0248b9cdc1c469ffe6fd2d311c.

--- project/source/global/Global.gd
+++ project/source/global/Global.gd
@@ -3,11 +3,6 @@ extends Node2D
 # References
 var player = null
 
-# Scenes
-var Menu = "res://source/interface/Menu.tscn"
-var Level1 = "res://source/levels/Level1.tscn"
-var Victory = "res://source/levels/Level1.tscn"
-
 # Resources
 var heartbeat_normal = load("res://audio/sounds/heartbeat_test.ogg")
 var heartbeat_faster = load("res://audio/sounds/heartbeat_faster_test.ogg")
--- project/source/terrain/Tree.gd
+++
@@ -1,5 +0,0 @@
-extends Sprite
-
-func _on_Area2D_body_entered(body):
-	if body == Global.player:
-		get_tree().change_scene(Global.Victory)
